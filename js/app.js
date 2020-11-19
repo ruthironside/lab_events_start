00cc69ba2b1c5740a20eb29567fb49fdbb7468e7
Neale Johnston <neale@team-johnston.com>
2020-11-19 16:21:50 +0000

    Extensions done

--- js/app.js
+++ js/app.js
@@ -1,3 +1,34 @@
 document.addEventListener('DOMContentLoaded', () => {
-  console.log('JavaScript loaded');
+
+  const form = document.querySelector('#new-item-form');
+  form.addEventListener('submit', handleFormSubmit);
+
+  const button = document.querySelector('#delete');
+  button.addEventListener('click', handleButtonClick);
+
 })
+
+const handleFormSubmit = function (event) {
+  event.preventDefault();
+  const newListItem = document.createElement('li');
+  newListItem.classList.add('flexbox');
+  const newTitleItem = document.createElement('h2');
+  const newAuthorItem = document.createElement('h3');
+  const newCategoryItem = document.createElement('h4');
+  newTitleItem.textContent = `Title: ${this.title.value}`
+  newAuthorItem.textContent = `Author: ${this.author.value}`
+  newCategoryItem.textContent = `Category: ${this.category.value}`
+
+  const list = document.querySelector('#reading-list');
+  list.appendChild(newListItem);
+  newListItem.appendChild(newTitleItem);
+  newListItem.appendChild(newAuthorItem);
+  newListItem.appendChild(newCategoryItem);
+
+  const form = document.querySelector('#new-item-form');
+  form.reset();
+}
+const handleButtonClick = function () {
+  console.log("button clicked");
+  window.location.reload();
+}
